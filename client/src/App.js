ee843a0c790262a8ee154e68506971c82afed50b
lgv-0 <55011072+lgv-0@users.noreply.github.com>
2020-02-14 12:33:02 -0500

    Cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import PlayerContainer from "./components/getPlayers";
 import useDarkMode from "./components/useDarkMode";

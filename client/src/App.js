c27023aa914c60ef89063f7094daedb96e98fa8f
lgv-0 <55011072+lgv-0@users.noreply.github.com>
2020-02-14 11:58:17 -0500

    Custom hook, dark mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import PlayerContainer from './components/getPlayers';
+import PlayerContainer from "./components/getPlayers";
+import useDarkMode from "./components/useDarkMode";
 
-function App() {
+function App()
+{
+  const [darkMode, setDarkMode] = useDarkMode(false);
+
+  let checkChange = function(e)
+  {
+    setDarkMode(e.target.checked);
+  }
+
   return (
     <div className="App">
+      <label htmlFor="darkchange">Dark-Mode</label>
+      <input type="checkbox" id="darkchange" onChange={checkChange} checked={darkMode} />
       <PlayerContainer />
     </div>
   );
